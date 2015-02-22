9a18f6b91171cf5f2bc80deddbe9a635df2c4261
Andrew James <andrewpjames87@gmail.com>
2015-02-22 09:30:28 -0700

    User can create Topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,4 +4,23 @@ class TopicsController < ApplicationController
     @topics = Topic.all
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+      if @topic.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
+
 end
